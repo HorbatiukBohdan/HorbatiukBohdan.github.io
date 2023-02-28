1f5fddfdedffea3621f63882f45802a6a24f08aa
pavelgronsky <pavelgronsky@gmail.com>
2023-02-28 23:31:49 +0100

    check props state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import s from"./Header.module.css"
+import React from 'react'
+import s from "./Header.module.css"
+
 const Header = () => {
-    return(
-    <header className={s.header}>
-    <img src='https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png' />
-  </header>
-   )}
-   export default Header;
+    return (
+        <header className={s.header}>
+            <img src='https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png' />
+        </header>
+    )
+}
+
+export default Header
